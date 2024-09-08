3b8c4fc43828b9dd9f6b79edcb4876a46dbad20e
DanielPuente <puente10@hotmail.com>
2024-09-07 22:06:58 -0600

    Initial Commit

---
+++ DoggoGuide/DoggoGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoggoGuideApp.swift
+//  DoggoGuide
+//
+//  Created by Daniel Puente on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct DoggoGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
